fb6096775e93ff34ebff7376dabd587b2bb3a730
Alan Selewa <selewa@midway2-login2.rcc.local>
2022-04-19 19:44:32 -0500

    added finemap prepare

---
+++ R/prepare_for_finemapping.R
@@ -0,0 +1,44 @@
+library(finemappeR)
+library(liftOver)
+library(ComplexHeatmap)
+source('R/analysis_utils.R')
+setwd('/project2/gca/aselewa/heart_atlas_project/')
+
+# Run torus on joint model
+rds <- readRDS('GWAS/summary_statistics/aFib/ebi-a-GCST006414_aFib.df.rds')
+annotations <- list.files(path = 'GWAS/annotations_for_finemapping_hg19/', pattern = '*.bed', full.names = T)
+
+cleaned.gwas.annots <- annotator(rds, annotations = annotations)
+
+annot_columns = colnames(cleaned.gwas.annots)[endsWith(x=colnames(cleaned.gwas.annots), suffix='bed_d')]
+cleaned.gwas.annots = cleaned.gwas.annots[,c("snp",annot_columns)]
+
+# get SNP prior
+readr::write_tsv(x = cleaned.gwas.annots, path = 'torus_annotations.txt.gz', col_names = T)
+readr::write_tsv(x = rds[,c('snp','locus','zscore')], path = 'torus_zscore.txt.gz', col_names = T)
+torus.result <- RunTorus(torus_annot_file = 'torus_annotations.txt.gz', torus_zscore_file = 'torus_zscore.txt.gz') 
+
+saveRDS(torus.result, file = 'GWAS/Torus_Enrichment_Results_Joint.rds')
+snp_torus_pip <- torus.result$snp_pip
+
+# get locus FDR
+torus.result.fdr <- RunTorusFDR(torus_annot_file = 'torus_annotations.txt.gz', torus_zscore_file = 'torus_zscore.txt.gz') 
+
+system('rm torus_annotations.txt.gz')
+system('rm torus_zscore.txt.gz')
+
+# add Torus PIPs to normalized GWAS and split by locus
+sumstats_finemap <- PrepareSusieData(sumstats,torus_pip = snp_pip, torus_fdr = torus.result.fdr, fdr_thresh = 0.1)
+
+prnt('Will finemap this many loci:')
+print(length(unique(sumstats_finemap$locus)))
+
+# save each loci to its own RDS
+loci <- unique(sumstats_finemap$locus)
+for(l in loci){
+  curr.sumtats <- sumstats[sumstats$locus == l,]
+  saveRDS(paste0('GWAS/finemapping/locus_files_finemapping/aFib_SignifLoci_',l,'.df.rds'))
+}
+
+# run finemapping on sbatch using R/run_finemapping.R script on each output from above
+
